6ac1eb9707a530d67015c0613114889b00cf4fdc
RevanchismFish <41475196+chauvinfish@users.noreply.github.com>
2021-08-11 06:11:32 +0800

    03

--- air hockey.py
+++ air hockey.py
@@ -14,9 +14,29 @@ class SimpleGameRes:
         self.buttonB = resImg.subsurface(pygame.Rect(160,0,40,40))
 
 
-class Circle:
-    def __init__(self,*args,**kwargs):
-        pass
+class Oval:
+    def __init__(self,*args):
+        if len(args)==2:
+            self.left = args[0][0]
+            self.top = args[0][1]
+            self.width = args[1][0]
+            self.height = args[1][1]
+        elif len(args)==4:
+            self.left = args[0];self.top = args[1];self.width=args[2];self.height=args[3]
+        elif len(args)==0:
+            self.left = 0;self.top = 0;self.width = 0;self.height = 0
+        else: raise ValueError
+
+    def move(self,dx,dy):
+        self.left = self.left+dx
+        self.top = self.top+dy
+
+    def collidepoint(self,pos):
+        ctx = self.left + self.width//2
+        cty = self.top + self.width//2
+        if pow(pos[0]-ctx,2)/pow(self.width//2,2) + pow(pos[1]-cty,2)/pow(self.height//2,2)<=1:
+            return True
+        return False
 
 class ButtonOnRelease:
     def __init__(self,clickArea):
@@ -54,10 +74,9 @@ class ButtonDraggable:
             self.dragging = False
         elif event.type==pygame.MOUSEBUTTONDOWN and self.clickArea.collidepoint(event.pos):
             self.dragging = True
-            print("dragging")
         elif event.type == pygame.MOUSEMOTION and self.dragging:
             self.effect_when_dragging(event)
-            print(event)
+
 
 
 class HockeyButton(ButtonDraggable):
@@ -67,7 +86,9 @@ class HockeyButton(ButtonDraggable):
         self.pos = pos
 
     def effect_when_dragging(self,event):
-        self.pos = event.pos
+        self.pos[0] = self.pos[0] + event.rel[0]
+        self.pos[1] = self.pos[1] + event.rel[1]
+        self.clickArea.move(event.rel[0],event.rel[1])
 
     def draw(self,targetSurface,pos=None):
         if pos == None:
@@ -100,8 +121,8 @@ class ActivableImageButton(ButtonOnRelease):
 
 res = SimpleGameRes()
 controlModeButton = ActivableImageButton(res.buttonA,pygame.Rect(0,0,40,40),res.buttonB)
-hockeyR = HockeyButton(res.hockeyR,pygame.Rect(0,0,40,40),[200,40])
-hockeyB = HockeyButton(res.hockeyB,pygame.Rect(0,0,40,40),[200,570])
+hockeyR = HockeyButton(res.hockeyR,Oval(200,40,40,40),[200,40])
+hockeyB = HockeyButton(res.hockeyB,Oval(200,570,40,40),[200,570])
 
 while(1):
     screen.fill((200,255,255))
@@ -111,7 +132,6 @@ while(1):
     hockeyB.draw(screen)
     controlModeButton.draw(screen,(0,0))
     for event in pygame.event.get():
-        #print(event)
         controlModeButton.listening(event)
         hockeyR.listening(event)
         hockeyB.listening(event)
